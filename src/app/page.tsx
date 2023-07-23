804dd4dbb30cf5682d2b9a07b9db236cb9e06ce4
agrippa kellum <ask252@cornell.edu>
2023-07-22 22:42:44 -0400

    new copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,22 +19,31 @@ export default function Home() {
           style={{ objectPosition: "bottom", objectFit: "cover" }}
         />
       </div>
-      <div className="bg-white rounded-xl p-6 m-12 max-w-md w-full">
+      <div className="bg-white rounded-xl p-6 m-12">
         <h1 className="text-2xl mb-3">Nimblefoot Research</h1>
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium
-          quam vulputate dignissim suspendisse in est ante in nibh. Sit amet
-          nulla facilisi morbi tempus iaculis urna. Egestas sed sed risus
-          pretium. Felis imperdiet proin fermentum leo vel orci. Consequat nisl
-          vel pretium lectus quam id leo in. Gravida dictum fusce ut placerat.
-          Pellentesque dignissim enim sit amet venenatis. Dolor sit amet
-          consectetur adipiscing elit pellentesque habitant morbi. Ipsum
-          faucibus vitae aliquet nec ullamcorper sit. Platea dictumst quisque
-          sagittis purus sit amet. Scelerisque eu ultrices vitae auctor eu.
+        <div className="whitespace-nowrap">
+          <div>
+            hihi. we are a small and mighty research and software group.
+          </div>
+          <div>we specialize in:</div>
+          <ul>
+            <li>- building defi, with great agility</li>
+            <li>- deploying medium/low-capacity trading opportunities</li>
+            <li>- friendship and love</li>
+          </ul>
+          <div>
+            our primary interest is Solana due to its exciting ecosystem and
+            architecture.
+          </div>
         </div>
+
         <div className="text-gray-400 text-center m-2">✧ ✧ ✧</div>
-        <div className="text-center">hihi@nimblefoot.xyz</div>
+        <div>
+          what should we stick our noses into next? let us know:{" "}
+          <b>hihi@nimblefoot.xyz</b>
+        </div>
+
+        <div></div>
       </div>
     </main>
   );
